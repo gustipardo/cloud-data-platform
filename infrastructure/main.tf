1a3e4b47dd78984ef8d568fd0eeaeb1345e8e9a3
gustipardo <gusm2003@gmail.com>
2026-04-23 16:53:30 +0000

    chore: add infrastructure notes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -342,3 +342,5 @@ resource "aws_sns_topic" "alerts" {
 # NOTE: Evaluate Graviton-based Lambda for cost savings
 
 # NOTE: Evaluate Graviton-based Lambda for cost savings
+
+# TODO: Add backup strategy for DynamoDB

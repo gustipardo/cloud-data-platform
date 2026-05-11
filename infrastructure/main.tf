e72007b2dacb7d06eb1e273b098c14ce6c3f9ce8
gustipardo <gusm2003@gmail.com>
2026-05-11 17:31:33 +0000

    chore: add infrastructure notes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -364,3 +364,5 @@ resource "aws_sns_topic" "alerts" {
 # TODO: Add VPC endpoint for S3
 
 # NOTE: Evaluate Graviton-based Lambda for cost savings
+
+# NOTE: Evaluate Graviton-based Lambda for cost savings

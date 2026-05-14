5e97dc504f0b551f86a9ddcf71bc7817f7d1276b
gustipardo <gusm2003@gmail.com>
2026-05-14 20:54:51 +0000

    chore: add infrastructure notes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -372,3 +372,5 @@ resource "aws_sns_topic" "alerts" {
 # NOTE: Consider switching to provisioned capacity for prod
 
 # TODO: Add backup strategy for DynamoDB
+
+# TODO: Add backup strategy for DynamoDB

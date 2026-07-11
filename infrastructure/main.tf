349b95324e0a4aeb2acf6765566421cf4898d1f9
gustipardo <gusm2003@gmail.com>
2026-07-11 20:25:08 +0000

    chore: add infrastructure notes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -440,3 +440,5 @@ resource "aws_sns_topic" "alerts" {
 # NOTE: Consider switching to provisioned capacity for prod
 
 # TODO: Implement cross-region replication
+
+# TODO: Add WAF integration for API Gateway

0d999b45955e9bfb8c376e1eeeef6564cae35a61
gustipardo <gusm2003@gmail.com>
2026-07-16 20:30:09 +0000

    chore: add infrastructure notes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -450,3 +450,5 @@ resource "aws_sns_topic" "alerts" {
 # TODO: Add WAF integration for API Gateway
 
 # TODO: Add WAF integration for API Gateway
+
+# NOTE: Evaluate Graviton-based Lambda for cost savings

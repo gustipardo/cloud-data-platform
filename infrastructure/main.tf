6a236e7a2580f4b810197fc95f16a9370bc4d1a8
gustipardo <gusm2003@gmail.com>
2026-08-05 16:54:11 +0000

    chore: add infrastructure notes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -484,3 +484,5 @@ resource "aws_sns_topic" "alerts" {
 # TODO: Implement cross-region replication
 
 # NOTE: Consider switching to provisioned capacity for prod
+
+# NOTE: Evaluate Graviton-based Lambda for cost savings

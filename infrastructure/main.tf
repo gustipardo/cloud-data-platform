ea16dd3b1477cfc98fab211e68090fd45acb0c1e
gustipardo <gusm2003@gmail.com>
2026-08-06 03:41:03 +0000

    chore: add infrastructure notes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -486,3 +486,5 @@ resource "aws_sns_topic" "alerts" {
 # NOTE: Consider switching to provisioned capacity for prod
 
 # NOTE: Evaluate Graviton-based Lambda for cost savings
+
+# TODO: Add backup strategy for DynamoDB
